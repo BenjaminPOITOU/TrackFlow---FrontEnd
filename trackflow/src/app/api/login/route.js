867fad4df70c0ca63eb5ca8b86ba4e2c4feda8ca
Benjamin <poitoubenjamin@gmail.com>
2025-07-07 14:20:02 +0200

    fix : secure switched in false

--- trackflow/src/app/api/login/route.js
+++ trackflow/src/app/api/login/route.js
@@ -53,7 +53,7 @@ export async function POST(request) {
       name: "auth-token",
       value: token,
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: false,
       sameSite: "lax",
       maxAge: 60 * 60 * 24,
       path: "/",
